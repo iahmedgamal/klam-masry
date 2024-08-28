4ff5bd79afaf5e664f6257d1e5d3bb097328f8e9
iahmedgamal <ahmad.g.mustafa@gmail.com>
2024-08-28 14:07:54 +0300

    loading

--- client/app/components/words/Words.tsx
+++ client/app/components/words/Words.tsx
@@ -10,7 +10,6 @@ interface WordsProps {
 }
 const Words = ({ currentPage, wordsPerPage }: WordsProps) => {
   const [words, setWords] = useState<Word[]>([]);
-
   console.log(process.env.NEXT_PUBLIC_API);
   useEffect(() => {
     fetch(process.env.NEXT_PUBLIC_API as string)
@@ -33,13 +32,14 @@ const Words = ({ currentPage, wordsPerPage }: WordsProps) => {
 
   return (
     <div className={styles.grid}>
+      {words.length === 0 && <div>Loading...</div>}
       {currentWords.map((item, index) => (
         <div key={item._id} className={styles.wordItem}>
           <Tooltip text={item.appeared}>
             <Link href={`/word/${item._id}`} passHref>
               <h2 className={styles.theWord}>{item.word} </h2>
-            <br></br>
-            <h2>{item.en}</h2>
+              <br></br>
+              <h2>{item.en}</h2>
             </Link>
           </Tooltip>
         </div>
